fb0b85e2e38a131c6edda660f13e60e999b0b859
Shuvam Mazumder <shuvammaz94@gmail.com>
2020-07-28 21:12:12 +0600

    learned basic math in JS

--- basicMath.js
+++ basicMath.js
@@ -1,6 +1,32 @@
-var price1 =35;
-var price2=25;
-var total= price1%price2;
-console.log(total);
-total++;
-console.log(total);
+// var price1 =35;
+// var price2=25;
+// var total= price1%price2;
+// console.log(total);
+// total++;
+// console.log(total);
+
+
+//math absolute
+var number1 =-5.5;
+var result1 = Math.abs(number1);
+console.log(result1);
+
+//math round
+var number2= 5.4;
+var result2 =Math.round(number2);
+console.log(result2);
+
+//math ceil
+var number3= -2.9;
+var result3 =Math.ceil(number3);
+console.log(result3);
+
+//math floor
+var number4 = -1.9;
+var result4= Math.floor(number4);
+console.log(result4);
+
+//math random
+var number5= Math.random()*2;
+var result5=Math.round(number5);
+console.log(result5);
