27f01514b92cbc9e85038dea1a518873755a29ab
Shuvam Mazumder <shuvammaz94@gmail.com>
2020-07-28 23:14:57 +0600

    learned array

---
+++ array.js
@@ -0,0 +1,17 @@
+var friendAge = [10,15,20,22,23];
+console.log(friendAge[0]);
+
+var crushAge = friendAge [2];
+console.log(crushAge);
+
+friendAge[1]= 17;
+console.log(friendAge[1]);
+
+var findPosition=friendAge.indexOf(23);
+console.log(findPosition);
+
+var findPosition=friendAge.indexOf(30);
+console.log(findPosition);
+
+
+
---
+++ if-else.js
@@ -0,0 +1,19 @@
+var price= 10;
+if (price != 10) {
+    console.log("i will eat biscuit");
+}
+else{
+    console.log("only Cha is better");
+}
+
+
+//multiple if else
+var jobPiaso=true;
+var savingAmount= 5000;
+
+if (jobPiaso == false && savingAmount > 20000) {
+    console.log("aso patri khuji");
+}
+else{
+    console.log("tor kopale biya nai");
+}
