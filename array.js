d7cc3a389221f030f380f487aa48715e0857bfbf
Shuvam Mazumder <shuvammaz94@gmail.com>
2020-07-28 23:30:42 +0600

    basic array

--- array.js
+++ array.js
@@ -1,17 +1,20 @@
-var friendAge = [10,15,20,22,23];
+var friendAge = [10, 15, 20, 22, 23];
 console.log(friendAge[0]);
 
-var crushAge = friendAge [2];
+var crushAge = friendAge[2];
 console.log(crushAge);
 
-friendAge[1]= 17;
+friendAge[1] = 17;
 console.log(friendAge[1]);
 
-var findPosition=friendAge.indexOf(23);
+var findPosition = friendAge.indexOf(23);
 console.log(findPosition);
 
-var findPosition=friendAge.indexOf(30);
+var findPosition = friendAge.indexOf(30);
 console.log(findPosition);
 
+friendAge.push(15);
+console.log(friendAge, friendAge.length);
 
-
+friendAge.pop();
+console.log(friendAge);
