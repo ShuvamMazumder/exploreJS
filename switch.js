c3ad5c51e93a6feddcd9b32a64eae42a34e48b67
Shuvam Mazumder <shuvammaz94@gmail.com>
2020-07-29 20:43:56 +0600

    learned switch

---
+++ switch.js
@@ -0,0 +1,17 @@
+var num=100;
+switch(num){
+    case 100:
+        console.log("im 100");
+        break;
+    case 50:
+    case 40:
+    case 30:
+        console.log("also im not 5");
+        break;
+    case 5:
+        console.log("u find out me");
+        break;
+    default :
+    console.log("u cant find me");
+
+}
