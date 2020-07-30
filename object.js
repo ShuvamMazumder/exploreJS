d4b3ee82a4fba1bfda5c0fb21ff013728ff1e72b
Shuvam Mazumder <shuvammaz94@gmail.com>
2020-07-30 14:47:58 +0600

    learned object

---
+++ object.js
@@ -0,0 +1,13 @@
+var student1 = {id: 110,phone: 88017,name: "shuvam"};
+var student2={id:150,phone:88015,name:"mahi"};
+console.log(student1);
+console.log(student2);
+student1.phone= 88016;
+
+var phNumber =student1.phone;
+console.log(phNumber);
+
+console.log(student1);
+
+student2.class = 10;
+console.log(student2);
