ee560f23ebc1840f50b5d615a51b8fe63dae153d
Shuvam Mazumder <shuvammaz94@gmail.com>
2020-07-30 20:26:36 +0600

    diff btw let and const

---
+++ constLet.js
@@ -0,0 +1,12 @@
+let name ="shuvam";
+if(name.length>4){
+    name="svm"
+}
+console.log(name);
+
+
+const name ="shuvam"; //const never change the value
+if(name.length>4){
+    name="svm"
+}
+console.log(name);
