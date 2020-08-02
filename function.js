d1e3511900620937c8a0faf5108cba393d3c8a7b
Shuvam Mazumder <shuvammaz94@gmail.com>
2020-08-03 00:50:50 +0600

    js

--- function.js
+++ function.js
@@ -1,13 +1,13 @@
-function hello() {
-  console.log("im shuvam");
-}
+// function hello() {
+//   console.log("im shuvam");
+// }
 
-console.log("hello jonny");
-hello();
-console.log("hello denny");
-hello();
-console.log("hello jen");
-hello();
+// console.log("hello jonny");
+// hello();
+// console.log("hello denny");
+// hello();
+// console.log("hello jen");
+// hello();
 
 // function doubleIt(num){
 //   var result =num*2;
@@ -17,20 +17,35 @@ hello();
 // doubleIt(5);
 // doubleIt(10);
 
-function doubleIt(num) {
-  var result = num * 2;
-  return result;
-}
-var first = doubleIt(5);
-var second = doubleIt(10);
-console.log(first, second);
+// function doubleIt(num) {
+//   var result = num * 2;
+//   return result;
+// }
+// var first = doubleIt(5);
+// var second = doubleIt(10);
+// console.log(first, second);
 
-var total = first + second;
-console.log(total);
+// var total = first + second;
+// console.log(total);
 
-function add(num1, num2) {
-  var result = num1 + num2;
-  return result;
+// function add(num1, num2) {
+//   var result = num1 + num2;
+//   return result;
+// }
+// var sum = add(15, 17);
+// console.log(sum);
+
+//call-back function
+function callBack(name, age, task) {
+    console.log("hello", name);
+    console.log("your age", age);
+    task();
 }
-var sum = add(15, 17);
-console.log(sum);
+function takeShower(){
+  console.log('take a shower');
+}
+function washHand() {
+    console.log("wash ur hand");
+}
+callBack("abir", 17,washHand);
+callBack("shuvam", 23,takeShower);
