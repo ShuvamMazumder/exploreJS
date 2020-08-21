2285707944f69f007690f8f26ac2900b77715126
Shuvam Mazumder <shuvammaz94@gmail.com>
2020-08-21 20:12:26 +0600

    arguments

--- arguments.js
+++ arguments.js
@@ -1,7 +1,22 @@
-function getFullName(firstName,lastName){
-    const fullName= firstName+' '+lastName;
-    return fullName;
+// function getFullName(firstName,lastName){
+//     const fullName= firstName+' '+lastName;
+//     return fullName;
 
+// }
+
+// const name= getFullName('shuvam', 'mazumder','loves','whom');
+// console.log(name);
+
+
+function getFullName(firstName,lastName){
+    var fullName='';
+    for (let i = 0; i < arguments.length; i++) {
+        const namePart = arguments[i];
+        fullName = fullName + ' '+ namePart;
+
+        
+    }
+    return fullName;
 }
 
 const name= getFullName('shuvam', 'mazumder','loves','whom');
