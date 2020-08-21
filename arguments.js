dc39a0a39f4bedc9496e0d88dca913b33a8a8171
Shuvam Mazumder <shuvammaz94@gmail.com>
2020-08-21 16:23:29 +0600

    arguments

---
+++ arguments.js
@@ -0,0 +1,8 @@
+function getFullName(firstName,lastName){
+    const fullName= firstName+' '+lastName;
+    return fullName;
+
+}
+
+const name= getFullName('shuvam', 'mazumder','loves','whom');
+console.log(name);
